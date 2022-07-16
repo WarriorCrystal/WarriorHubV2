93ddf69dacfe28b6051c65288599cadfc5580dea
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2022-07-16 14:39:37 -0300

    Update Shit.lua

--- Shit.lua
+++ Shit.lua
@@ -1,2 +1,2 @@
-getgenv().Credits = {"Credits", "Vynixu (Discord Lib)", "Venus UI Lib Dev", "WarriorCrystal"}
+getgenv().Credits = {"Credits", "Vynixu (Discord Lib)", "Venus UI Lib Dev", "Some random guy for invis", "WarriorCrystal"}
 getgenv().Donators = {"Donators", "naruto10123n", "GalazyZane", "PepeeNob", "LoriDev", "orba102 / H5CK"}
